43d8fae27ba106dff986ccb55cb030f5c81e1440
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-06-23 13:58:16 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="py-10 flex flex-col items-center justify-center gap-6 leading-6 text-dark-blue">
+      <ul className="">
+        <li>
+          <a href="">Home</a>
+        </li>
+      </ul>
+      <p className="">@2024 BuildRight. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
