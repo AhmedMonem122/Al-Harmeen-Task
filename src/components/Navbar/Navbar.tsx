bba1e2c8c31c07378f3e3945597592fdcfc28153
AhmedMonem122 <ahmed.monem90@yahoo.com>
2025-06-23 13:24:57 +0300

    style: update Navbar styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import { BiSolidCylinder } from "react-icons/bi";
 
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 bg-dark-white py-3 xxl:px-10 border-b border-light-gray">
+    <nav className="sticky top-0 bg-dark-white py-3 px-2.5 sm:px-0 xxl:px-10 border-b border-light-gray">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
           <a href="" className="text-dark-black flex items-center gap-4">
@@ -25,7 +25,7 @@ const Navbar = () => {
             <li>
               <Button
                 asChild
-                className="bg-primary-blue px-4 rounded-[8px] font-bold text-[14px] leading-[21px] text-dark-white hover:bg-primary-blue/90"
+                className="px-4 rounded-[8px] text-[14px] leading-[21px] hover:text-dark-white font-bold relative z-10 after:content-['Login'] after:opacity-0 hover:after:opacity-100 after:flex after:items-center after:justify-center after:absolute after:w-full after:h-0 after:bg-primary-blue after:bottom-0 hover:after:h-full after:rounded-[8px] after:transition-all after:duration-300 after:ease-in-out"
               >
                 <a href="/login">Login</a>
               </Button>
